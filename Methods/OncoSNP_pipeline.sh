d569b60d8ac4d7e5c066ee0b7fd935335c8043db
adspit <7578854+adspit@users.noreply.github.com>
2017-11-17 14:28:48 +0100

    Update OncoSNP_pipeline.sh

--- Methods/OncoSNP_pipeline.sh
+++ Methods/OncoSNP_pipeline.sh
@@ -3,7 +3,7 @@
 # cd PATH_TO_ONCOSNP - change path to directory where you have installed OncoSNP or give path to the run_oncosnp.sh script
 # batchx$SGE_TASK_ID.txt represents the batch file - split the big batch file into 80 batches each of 5 samples - 
 # so we can run OncoSNP in parallel
-# the normalcontentlevels are set to 0, 0.3, 0.5 and 0.7
+# the normalcontentlevels are set to 0 - here, and were replaced with 0.3, 0.5 and 0.7 for the corresponding samples
 
 cd PATH_TO_ONCOSNP &&
 ./run_oncosnp.sh v82/ --batch-file batchx$SGE_TASK_ID.txt --output-dir oncosnp_rez \\
